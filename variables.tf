e5c16d92d75de272b06710a1170d769c224817f2
Mzembaba2002 <mzembabafardati70@gmail.com>
2026-04-21 20:17:24 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,54 @@
+variable "region" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
 variable "environment" {
-  description = "Deployment environment"
-  type        = string
-  default     = "dev"
+  type    = string
+  default = "dev"
+}
+
+variable "kubernetes_version" {
+  type    = string
+  default = "1.33"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "instance_types" {
+  type    = list(string)
+  default = ["t3.medium"]
+}
+
+variable "node_desired_size" {
+  type    = number
+  default = 2
+}
+
+variable "node_min_size" {
+  type    = number
+  default = 1
+}
+
+variable "node_max_size" {
+  type    = number
+  default = 3
 }
