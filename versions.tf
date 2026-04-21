2d421620f7c27c9e181d93d93f5c42eb0d025aeb
Mzembaba2002 <mzembabafardati70@gmail.com>
2026-04-21 20:16:44 +0000

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,10 +1,21 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.6.0"
+
+  backend "s3" {
+    bucket       = "MON-BUCKET-TFSTATE"
+    key          = "eks/terraform.tfstate"
+    region       = "eu-west-3"
+    encrypt      = true
+    use_lockfile = true
+  }
 
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.5"
+    aws = {
+      source = "hashicorp/aws"
     }
   }
+}
+
+provider "aws" {
+  region = var.region
 }
