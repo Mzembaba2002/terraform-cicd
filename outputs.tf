b90c8ceed4f16236a4091aa490501cb636345769
Mzembaba2002 <mzembabafardati70@gmail.com>
2026-04-21 20:16:06 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
-output "file_name" {
-  value = local_file.example.filename
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "region" {
+  value = var.region
 }
